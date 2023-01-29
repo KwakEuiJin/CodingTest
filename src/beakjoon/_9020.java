e57908e41af4a0b6e3ccbcca4b27da2a4d11ea01
곽의진 <yuijin0909@cashwalk.io>
2023-01-29 22:26:48 +0900

    문제풀이 2023/1/29

--- src/beakjoon/_9020.java
+++ src/beakjoon/_9020.java
@@ -1,7 +1,39 @@
 package beakjoon;
 
-public class _9020 {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class _9020 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        String[] answer = new String[T];
+        boolean[] arr = new boolean[10000];
+
+        for (int i = 2; i < arr.length - 1; i++) {
+            for (int j = i * i; j < arr.length - 1; j += i) {
+                arr[j] = true;
+            }
+        }
+
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int a = n / 2;
+            int b = n / 2;
+
+            while (true) {
+                if (!arr[a] && !arr[b]) {
+                    answer[i] = String.format("%d %d", a, b);
+                    break;
+                }
+                a--;
+                b++;
+            }
+        }
+
+        for (int i = 0; i < T; i++) {
+            System.out.println(answer[i]);
+        }
     }
 }
--- src/programmers_lv1/성격 유형 검사하기.kt
+++ src/programmers_lv1/성격 유형 검사하기.kt
@@ -1,2 +1,38 @@
 package programmers_lv1
 
+import java.lang.StringBuilder
+import kotlin.math.max
+
+fun main() {
+    println(solution(arrayOf("AN", "CF", "MJ", "RT", "NA"), intArrayOf(5, 3, 2, 7, 5)))
+}
+
+private fun solution(survey: Array<String>, choices: IntArray): String {
+    var answer = StringBuilder("")
+    val surveyList = listOf('R', 'T', 'C', 'F', 'J', 'M', 'A', 'N')
+    val surveyResultList = mutableListOf(0, 0, 0, 0, 0, 0, 0, 0)
+
+    survey.forEachIndexed { index, k ->
+        if (choices[index] > 4) {
+            surveyResultList[surveyList.indexOf(k[1])] += choices[index] -4
+        } else if (choices[index] < 4) {
+            val cost = when(choices[index]) {
+                1 -> 3
+                2-> 2
+                else -> 1
+            }
+            surveyResultList[surveyList.indexOf(k[0])] += cost
+        }
+    }
+
+    for (i in 0..3) {
+        if (surveyResultList[i * 2] > surveyResultList[(i * 2) + 1]) {
+            answer.append(surveyList[i * 2])
+        } else if (surveyResultList[i * 2] < surveyResultList[(i * 2) + 1]) {
+            answer.append(surveyList[(i * 2) + 1])
+        } else {
+            answer.append(minOf(surveyList[i * 2], surveyList[(i * 2) + 1]))
+        }
+    }
+    return answer.toString()
+}
